879107169a93e12dc2c8dc23b7f6888ebee5b041
Ravindra Kumar <ravindrakumar15075@gmail.com>
2024-09-22 17:53:22 +0530

    Animation Btn

--- main.js
+++ main.js
@@ -1,38 +1,46 @@
-// const inputBox = document.querySelector("#input-box");
-// const listContainer= document.querySelector("#list-container");
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
-function addTask(){
-         if(inputBox.value === ''){
-            alert("You must write something!");
-         }
-         else{
-            let li = document.createElement("li");
-            li.innerHTML = inputBox.value;
-            listContainer.appendChild(li);
-            let span = document.createElement("span");
-            span.innerHTML = "\u00d7";
-            li.appendChild(span);
-         }
-         inputBox.value = "";
-         saveData();  
+const inputBox = document.querySelector("#input-box");
+const listContainer= document.querySelector("#list-container");
+const addButton = document.querySelector("button");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+
+    // Add animation class to the button
+    addButton.classList.add("animate-btn");
+
+    // Remove the animation class after the animation ends (0.3 seconds)
+    setTimeout(function () {
+        addButton.classList.remove("animate-btn");
+    }, 300);  // Duration matches the animation in the CSS
 }
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
     }
-    else if(e.target.tagName === "SPAN"){
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
-},false); 
+}, false);
 
-function saveData(){
+function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
-} 
+}
 
-function showTask(){
+function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
